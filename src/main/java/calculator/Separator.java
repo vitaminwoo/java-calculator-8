df4534f2011e48d8d90b699de3ea0c73b1529756
minwoo_park <nwpark111@gmail.com>
2025-10-17 20:51:45 +0900

    feat<separator>: add getNums
    1.2 숫자 추출 (잘못된 값 입력 예외 처리 포함)
    
    입력 받은 문자열에서 숫자들을 추출하는 메서드 getNums 구현.

--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -13,6 +13,27 @@ public class Separator {
         regex_str = ",|:"; // 구분자 필드로 따로 명시.
     }
 
-//    int[] getNums() {}
+    int[] getNums(String add_str) {
+        String[] nums_str = add_str.split(regex_str); // 이 구분자를 따로 변수로 명시해주면 더 좋을듯.정규표현식이라는 개념.
+        int[] nums = new int[nums_str.length];
+
+        try {
+            for (int i = 0; i < nums_str.length; i++) {
+                int num = Integer.parseInt(nums_str[i]);
+                if (num < 0) {
+                    throw new IllegalArgumentException();
+                }
+                //            System.out.println(num);
+                nums[i] = num;
+            }
+        } catch (IllegalArgumentException e) {
+            nums = null;
+            System.out.println("IllegalArgumentException");
+        } catch (Exception e) {
+            nums = null;
+        }
+        return nums;
+
+    }
 
 }
