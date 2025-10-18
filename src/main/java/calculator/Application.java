663cc7264315572c990ad0c898a8af8c5d5ed258
minwoo_park <nwpark111@gmail.com>
2025-10-18 23:34:11 +0900

    feat<main> add main class
    프로그램 구현에 대한 메인 클래스 코드 작성.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -1,7 +1,22 @@
 package calculator;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class Application {
     public static void main(String[] args) {
         // TODO: 프로그램 구현
+        String add_str = Console.readLine();
+
+        Separator separator = new Separator(add_str);
+        separator.setRegex();
+        separator.checkCustomRegex();
+        int[] intList = separator.getNums();
+
+        if (intList != null) {
+            Calculator calculator = new Calculator(intList);
+            int result = calculator.plusNums();
+            System.out.println("결과 : " + result);
+        }
+
     }
 }
