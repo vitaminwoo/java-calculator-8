4584c91b996c1b0d3a90342e28829e04ed233003
minwoo_park <nwpark111@gmail.com>
2025-10-19 17:28:52 +0900

    refactor<separator> refactor method and field
    불필요한 상태 유지 필드 삭제 및 메서드의 정적화 구현.
    
    split에 필요한 구분자 처리에 대해, 필드가 지나치게 많고 메인에서 호출해야 하는 메서드도 많아 해당 부분을 리팩토링했다. 정적 메서드로 구현하여 인스턴스 생성을 줄이며, Separate 클래스 자체에서 메서드 간 호출을 하여 메인에서는 하지 않도록 수정하였다.

--- src/main/java/calculator/Application.java
+++ src/main/java/calculator/Application.java
@@ -4,13 +4,9 @@ import camp.nextstep.edu.missionutils.Console;
 
 public class Application {
     public static void main(String[] args) {
-        // TODO: 프로그램 구현
         String add_str = Console.readLine();
 
-        Separator separator = new Separator(add_str);
-        separator.setRegex();
-        separator.checkCustomRegex();
-        int[] intList = separator.getNums();
+        int[] intList = Separator.separate(add_str);
 
         Calculator calculator = new Calculator(intList);
         int result = calculator.plusNums();
--- src/main/java/calculator/Separator.java
+++ src/main/java/calculator/Separator.java
@@ -2,39 +2,30 @@ package calculator;
 
 public class Separator {
 
-    String input_str;
-    String regex_str;
-    String custom_regex;
+    private static final String REGEX_DEFAULT = ",|:";
 
-    Separator(String input_str) {
-        this.input_str = input_str;
-    }
+    public static int [] separate(String input_str) {
+        String regex_str = REGEX_DEFAULT;
 
-    void setRegex() {
-        regex_str = ",|:"; // 구분자를 정규식 형태로 정의.
-    }
-
-    void checkCustomRegex() {
         String regex_check = "//|\\\\n";
         String[] check_custom = input_str.split(regex_check);
         if (check_custom.length != 1) {
-            custom_regex = check_custom[1];
-            regex_str = regex_str + "|" + custom_regex;
+            regex_str = regex_str + "|" + check_custom[1];
             input_str = check_custom[2];
         }
+
+        String[] nums_str = input_str.split(regex_str);
+        return getNums(nums_str);
     }
 
-    int[] getNums() {
-        String[] nums_str = input_str.split(regex_str); // 정규식 형태로 정의된 구분자 삽입 후 split.
+    private static int[] getNums(String[] nums_str) {
         int[] nums = new int[nums_str.length];
-
 
         for (int i = 0; i < nums_str.length; i++) {
                 int num = Integer.parseInt(nums_str[i]);
                 if (num < 0) {
                     throw new IllegalArgumentException();
                 }
-                //            System.out.println(num);
                 nums[i] = num;
         }
         return nums;
